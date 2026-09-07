a468fe391a1db63ca0e2fbcb14e0fa308e2ae4ed
GaneshN523 <ganeshnalawade523@gmail.com>
2026-09-07 23:05:49 +0530

    Create clearn home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,9 @@
 export default function Home() {
   return (
-    <main className="min-h-screen flex items-center justify-center">
-      <div className="text-center">
-        <h1 className="text-4xl font-bold">
-          My First Next.js Website 🚀
-        </h1>
-
-        <p className="mt-4 text-gray-600">
-          Built with JavaScript and deployed on Vercel.
-        </p>
-      </div>
+    <main>
+      <h1>RePromptsQuest</h1>
+      <p>Welcome to my website.</p>
     </main>
   );
 }
+
