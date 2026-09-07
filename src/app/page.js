29751d8f2dad6cfa38e0b2f692ffe948ad42a08c
GaneshN523 <ganeshnalawade523@gmail.com>
2026-09-07 22:28:25 +0530

    Initial Next.js app

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold">
+          My First Next.js Website 🚀
+        </h1>
+
+        <p className="mt-4 text-gray-600">
+          Built with JavaScript and deployed on Vercel.
+        </p>
+      </div>
+    </main>
+  );
+}
